8b96def36365de1f684339954f139f91713f6e78
Andy Rigby <andy.rigby@ixis.co.uk>
2016-06-16 12:07:21 +0100

    Add Page object for editing an entity queue - simple only

---
+++ src/Drupal/Pages/AdminEntityQueueSimpleEditPage.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @file
+ * Admin page for Structure.
+ */
+
+namespace Codeception\Module\Drupal\Pages;
+
+/**
+ * Class AdminEntityQueueEditPage
+ */
+class AdminEntityQueueSimpleEditPage
+{
+    /**
+     * Base URL/path to this page.
+     */
+    const URL = 'admin/structure/entityqueue/list/%s/subqueues/1/edit';
+
+    /**
+     * @var string
+     *   Selector for the autocomplete text field.
+     */
+    public static $autoCompleteSelector = ".entityqueue-dragtable-eq-file-add.form-autocomplete";
+
+    /**
+     * @var string
+     *   Selector to determine if the entity is in the queue.
+     */
+    protected static $titleInQueueSelector = "//td/a[.='%s']";
+
+    /**
+     * @param string $title
+     *   The entity title.
+     *
+     * @return string
+     *   The selector to determine if the entity is in the queue.
+     */
+    public static function elementInQueueSelector($title)
+    {
+        return sprintf(self::$titleInQueueSelector, $title);
+    }
+
+    /**
+     * Return an entity queue path.
+     *
+     * @param string $queue_name
+     *   Entity queue name.
+     *
+     * @return string
+     *   The path.
+     */
+    public static function route($queue_name)
+    {
+        return sprintf(self::URL, $queue_name);
+    }
+}
